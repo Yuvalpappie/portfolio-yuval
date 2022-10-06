a1c3a7120d743b08022a9e01f32c3808532092ff
Yuval Pappie <yuval_p@hotmail.com>
2022-10-06 10:24:41 +0200

    page transition mobile fix

--- components/Transitions.js
+++ components/Transitions.js
@@ -1,8 +1,27 @@
 import { motion, AnimatePresence } from 'framer-motion';
 import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
 
 const Transition = ({ children }) => {
     const { asPath } = useRouter();
+
+    const [width, setWidth] = useState(0)
+
+    useEffect(() => {
+        function handleResize() {
+            setWidth(window.innerWidth)
+        }
+
+        window.addEventListener("resize", handleResize)
+
+        handleResize()
+
+        return () => {
+            window.removeEventListener("resize", handleResize)
+        }
+    }, [setWidth])
+
+    console.log(width);
 
     const variants = {
         out: {
@@ -32,7 +51,7 @@ const Transition = ({ children }) => {
                     animate="in"
                     initial="out"
                     exit="out"
-                    key={asPath.startsWith("/#") ? '' : asPath}
+                    key={asPath.startsWith("/#") || width < 700 ? '' : asPath}
                 >
                     {children}
                 </motion.div>
